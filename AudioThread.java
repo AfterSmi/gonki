1a90d4efbe440399a7b5c27c0e1714fef600a0db
aftersmi <aftersmi@mail.ru>
2018-06-01 08:38:58 +0300

    1

---
+++ AudioThread.java
@@ -0,0 +1,28 @@
+//package car;
+//
+//import javazoom.jl.decoder.JavaLayerException;
+//
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//
+//public class AudioThread implements Runnable {
+//
+//	@Override
+//	public void run() {
+//
+//		try {
+//			Player p = new Player(new FileInputStream("res/cake.mp3"));
+//					p.play();
+//					Player b = new Player(new FileInputStream("res/cake.mp3"));
+//					b.play();
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (JavaLayerException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//	}
+//
+//}
